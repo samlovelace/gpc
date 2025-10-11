3a664ebf131cd2095795b754fc6faf8e6cf1001a
samlovelace <samlovelace12@gmail.com>
2025-10-10 21:11:17 -0400

    basic test framework

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+
+#include <gtest/gtest.h> 
+
+TEST(Testing, Test1)
+{
+    EXPECT_EQ(2+2, 4); 
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS(); 
+}
